6dfc86f207ce6e98c7f6cc4890a658c31c77ced7
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-22 19:16:21 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+//razzle()
+//function razzle(){
+ //   console.log("You've been razzled!");
+//}
+
+//function razzle(lawyer = "Billy", target = "'em"){
+//    console.log(`${lawyer} razzle-dazzles ${target}!`);
+//}
+//razzle()
+
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+//(function (){
+//    return("Yet more razzling");
+//});
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+  }
+  mondayWork('work from home');
+
+
+//Lab
+function wrapAdjective(par1="*"){
+    return function (par="a hard worker"){
+        return `You are ${par1}${par}${par1}!`
+    };
+  }
+  wrapAdjective("||") ("passionate in programming");
+
